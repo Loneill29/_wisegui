8348faa3954260e2923b00555eacd27c4c6db560
Linda <oneill.linda21@gmail.com>
2018-10-23 15:18:37 -0500

    add wikis controller/views

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,7 +31,8 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
-
+  config.include Devise::TestHelpers, type: :controller
+  config.include Devise::TestHelpers, type: :view
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
